482647b0d14247657e7a4fb30c744d4e0a7c8057
Alexandre <alexandre.gcosta@gmail.com>
2015-07-04 16:19:57 -0300

    5.10 Adding Some Validation

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,22 +3,26 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
-	def show
-		@article = Article.find(params[:id])
-	end
+    def show
+        @article = Article.find(params[:id])
+    end
 
- 	def new
-  	end
+    def new
+        @article = Article.new
+    end
 
-	def create
-		@article = Article.new(article_params)
+    def create
+        @article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
-	end
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
 
 private
-	def article_params
-		params.require(:article).permit(:title, :text)
-	end
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
 end
